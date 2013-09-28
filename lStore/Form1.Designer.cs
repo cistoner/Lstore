826f00e6abcc925bb7a2265c45bf22807f533b69
minhaz <mebjas@gmail.com>
2013-09-28 19:15:22 +0530

    somethings changed

--- lStore/Form1.Designer.cs
+++ lStore/Form1.Designer.cs
@@ -512,11 +512,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(558, 559);
+            this.button1.Location = new System.Drawing.Point(1097, 583);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 22;
-            this.button1.Text = "button1";
+            this.button1.Text = "test form 2";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
