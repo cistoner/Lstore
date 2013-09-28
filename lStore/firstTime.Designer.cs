921b9c0020a1ca9ffb6ff48a5e5aaa10f3c3e3c9
minhaz <mebjas@gmail.com>
2013-09-28 19:38:10 +0530

    UI of firstTime form changed :)
    looking cool now

--- lStore/firstTime.Designer.cs
+++ lStore/firstTime.Designer.cs
@@ -30,6 +30,13 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(firstTime));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.stepCount = new System.Windows.Forms.Label();
+            this.progress = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.rectangleShape1 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,22 +49,113 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Century Schoolbook", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(70, 262);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Step";
+            // 
+            // stepCount
+            // 
+            this.stepCount.AutoSize = true;
+            this.stepCount.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.stepCount.Font = new System.Drawing.Font("Lucida Sans Unicode", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stepCount.Location = new System.Drawing.Point(12, 307);
+            this.stepCount.Name = "stepCount";
+            this.stepCount.Size = new System.Drawing.Size(192, 59);
+            this.stepCount.TabIndex = 2;
+            this.stepCount.Text = "1 of 10";
+            // 
+            // progress
+            // 
+            this.progress.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.progress.Location = new System.Drawing.Point(331, 344);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(478, 22);
+            this.progress.TabIndex = 3;
+            this.progress.Value = 30;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(332, 307);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(603, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tip: Always rate users on basis of contents for better performance";
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.rectangleShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(947, 439);
+            this.shapeContainer1.TabIndex = 5;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // rectangleShape1
+            // 
+            this.rectangleShape1.BackColor = System.Drawing.Color.PeachPuff;
+            this.rectangleShape1.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.rectangleShape1.BorderColor = System.Drawing.Color.Transparent;
+            this.rectangleShape1.Location = new System.Drawing.Point(0, 412);
+            this.rectangleShape1.Name = "rectangleShape1";
+            this.rectangleShape1.Size = new System.Drawing.Size(947, 27);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkRed;
+            this.label3.Location = new System.Drawing.Point(4, 419);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(548, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Do not close this window, lStore would not work untill you have syncronized data " +
+    "with server!!";
+            // 
             // firstTime
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(947, 439);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progress);
+            this.Controls.Add(this.stepCount);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.shapeContainer1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(963, 478);
+            this.MinimumSize = new System.Drawing.Size(963, 478);
             this.Name = "firstTime";
             this.Text = "Syncronising with server";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label stepCount;
+        private System.Windows.Forms.ProgressBar progress;
+        private System.Windows.Forms.Label label2;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape1;
+        private System.Windows.Forms.Label label3;
     }
 }
