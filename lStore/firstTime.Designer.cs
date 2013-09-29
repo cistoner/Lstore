996e8b9ee7b4a308ae036f9722a20cdcb0aaadaa
minhaz <mebjas@gmail.com>
2013-09-30 00:41:29 +0530

    changes to firsttime UI and functionality
    2 background workers added to do local and online syncing

--- lStore/firstTime.Designer.cs
+++ lStore/firstTime.Designer.cs
@@ -37,6 +37,9 @@
             this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
             this.rectangleShape1 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
             this.label3 = new System.Windows.Forms.Label();
+            this.onlinesync = new System.ComponentModel.BackgroundWorker();
+            this.localsync = new System.ComponentModel.BackgroundWorker();
+            this.localSyncLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -69,7 +72,7 @@
             this.stepCount.Name = "stepCount";
             this.stepCount.Size = new System.Drawing.Size(192, 59);
             this.stepCount.TabIndex = 2;
-            this.stepCount.Text = "1 of 10";
+            this.stepCount.Text = "0 of 10";
             // 
             // progress
             // 
@@ -78,13 +81,12 @@
             this.progress.Name = "progress";
             this.progress.Size = new System.Drawing.Size(478, 22);
             this.progress.TabIndex = 3;
-            this.progress.Value = 30;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(332, 307);
+            this.label2.Location = new System.Drawing.Point(325, 309);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(603, 19);
             this.label2.TabIndex = 4;
@@ -116,12 +118,39 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.DarkRed;
-            this.label3.Location = new System.Drawing.Point(4, 419);
+            this.label3.Location = new System.Drawing.Point(5, 417);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(548, 16);
             this.label3.TabIndex = 6;
             this.label3.Text = "Do not close this window, lStore would not work untill you have syncronized data " +
     "with server!!";
+            // 
+            // onlinesync
+            // 
+            this.onlinesync.WorkerReportsProgress = true;
+            this.onlinesync.WorkerSupportsCancellation = true;
+            this.onlinesync.DoWork += new System.ComponentModel.DoWorkEventHandler(this.onlinesync_DoWork);
+            this.onlinesync.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.onlinesync_ProgressChanged);
+            // 
+            // localsync
+            // 
+            this.localsync.WorkerReportsProgress = true;
+            this.localsync.WorkerSupportsCancellation = true;
+            this.localsync.DoWork += new System.ComponentModel.DoWorkEventHandler(this.localsync_DoWork);
+            this.localsync.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.localsync_ProgressChanged);
+            this.localsync.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.localsync_RunWorkerCompleted);
+            // 
+            // localSyncLabel
+            // 
+            this.localSyncLabel.AutoSize = true;
+            this.localSyncLabel.BackColor = System.Drawing.Color.Transparent;
+            this.localSyncLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.localSyncLabel.ForeColor = System.Drawing.Color.DarkCyan;
+            this.localSyncLabel.Location = new System.Drawing.Point(760, 415);
+            this.localSyncLabel.Name = "localSyncLabel";
+            this.localSyncLabel.Size = new System.Drawing.Size(182, 20);
+            this.localSyncLabel.TabIndex = 7;
+            this.localSyncLabel.Text = "loading data to local files";
             // 
             // firstTime
             // 
@@ -129,6 +158,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(947, 439);
+            this.Controls.Add(this.localSyncLabel);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.progress);
@@ -136,8 +166,11 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.shapeContainer1);
+            this.Cursor = System.Windows.Forms.Cursors.AppStarting;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(963, 478);
+            this.MinimizeBox = false;
             this.MinimumSize = new System.Drawing.Size(963, 478);
             this.Name = "firstTime";
             this.Text = "Syncronising with server";
@@ -157,5 +190,8 @@
         private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
         private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape1;
         private System.Windows.Forms.Label label3;
+        private System.ComponentModel.BackgroundWorker onlinesync;
+        private System.ComponentModel.BackgroundWorker localsync;
+        private System.Windows.Forms.Label localSyncLabel;
     }
 }
--- lStore/firstTime.cs
+++ lStore/firstTime.cs
@@ -7,15 +7,140 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.IO;
 
 namespace lStore
 {
     public partial class firstTime : Form
     {
+        public int step = 1;
+        public string url = "http://www.cistoner.com/abcd/sync.php";
+        public bool isFirst = false;
+        private bool[] isFileDownloaded = new bool[10];
+        public string primaryFolder = @"C:\Users\" + Environment.UserName + @"\Documents\lStore";
         public firstTime()
         {
             InitializeComponent();
+            localSyncLabel.Visible = false;
+            stepCount.Text = " 1 of 10 ";
+            for (int i = 0; i < 10; i++)
+            {
+                isFileDownloaded[i] = false;    //for remoiving exceptio
+            }
+            onlinesync.RunWorkerAsync();
+            
         }
+        /* 
+         * main working fnction of
+         * backgroundworker that downloads data from server
+         */
+        private void onlinesync_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (step <= 10)
+            {
+                string tmp = SendPost(url, "step=" + step.ToString());
+                File.WriteAllText(primaryFolder + @"\tmp\data_step_" + step.ToString() +".data", tmp);
+                isFileDownloaded[step - 1] = true;
+                onlinesync.ReportProgress(step * 5);
+                step++;
+            }
+        }
+        private void onlinesync_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progress.Value += e.ProgressPercentage;
+            if (!isFirst) 
+            {
+                isFirst = true;
+                localSyncLabel.Visible = true;
+                localsync.RunWorkerAsync();
+            }
+            stepCount.Text = " " +step.ToString() + " of 10 ";
+        }
+        /*
+         * background worker to read data line by line from file
+         * and save that to db
+         */
+        private void localsync_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int count = 1;
+            while (count <= 10)
+            {
+                while (!File.Exists(primaryFolder + @"\tmp\data_step_" + count.ToString() + ".data") && isFileDownloaded[count-1]) { };
+                //this waits till the file has been created
+                
+                string[] files = File.ReadAllLines(primaryFolder + @"\tmp\data_step_" + count.ToString() + ".data");
+                for (int i = 0; i < files.Length; i++)
+                { 
+                    /* db query to
+                     * save this content to local db
+                     */ 
+                }
+                localsync.ReportProgress(count * 5);
+                count++;
+            }
+            
+        }
+        private void localsync_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progress.Value += e.ProgressPercentage;
+        }
+        private void localsync_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            /* 
+             * this means local syncing is over 
+             * by this time configuration files should 
+             * have been made
+             */
+            localSyncLabel.Visible = false;
+        }
+
+        /*
+         * function to send post request to any page and retrieve the content
+         */ 
+        public string SendPost(string url, string postData)
+        {
+            string webpageContent = string.Empty;
+
+            try
+            {
+                byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+                HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
+                webRequest.Method = "POST";
+                webRequest.ContentType = "application/x-www-form-urlencoded";
+                webRequest.ContentLength = byteArray.Length;
+
+                using (Stream webpageStream = webRequest.GetRequestStream())
+                {
+                    webpageStream.Write(byteArray, 0, byteArray.Length);
+                }
+
+                using (HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse())
+                {
+                    using (StreamReader reader = new StreamReader(webResponse.GetResponseStream()))
+                    {
+                        webpageContent = reader.ReadToEnd();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //throw or return an appropriate response/exception
+            }
+            return webpageContent;
+        }
+
+        
+
+        
+
+        
+
+        
+
+        
 
         
     }
